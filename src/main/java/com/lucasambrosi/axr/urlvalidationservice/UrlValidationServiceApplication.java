53618ed58db6c14fe712947a0fdf95f8e6d64f73
Lucas Ambrosi Derrossi <lucas.derrossi@ilegra.com>
2019-04-17 00:30:34 -0300

    Added initial project structure

---
+++ src/main/java/com/lucasambrosi/axr/urlvalidationservice/UrlValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lucasambrosi.axr.urlvalidationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlValidationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlValidationServiceApplication.class, args);
+    }
+
+}
