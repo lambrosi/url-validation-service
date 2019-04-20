e893c89012f98f2aaf792660a49c691936c1181c
Lucas Ambrosi Derrossi <lucas.derrossi@ilegra.com>
2019-04-20 06:50:09 -0300

    Added listener to test message receiving

--- src/main/java/com/lucasambrosi/axr/urlvalidationservice/messaging/RabbitConsumer.java
+++ src/main/java/com/lucasambrosi/axr/urlvalidationservice/messaging/RabbitConsumer.java
@@ -1,7 +1,26 @@
 package com.lucasambrosi.axr.urlvalidationservice.messaging;
 
+import com.lucasambrosi.axr.urlvalidationservice.input.RegexInput;
+import com.lucasambrosi.axr.urlvalidationservice.input.ValidationInput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitConsumer.class);
+
+    @RabbitListener(queues = "${INSERTION_QUEUE}", containerFactory = "genericListenerContainerFactory")
+    public void insertRegex(@Payload final RegexInput regexInput) {
+        LOGGER.info("Received message {}.", regexInput.toString());
+    }
+
+    @RabbitListener(queues = "${VALIDATION_QUEUE}", containerFactory = "validationListenerContainerFactory")
+    public void validateUrl(@Payload final ValidationInput validationInput) {
+        LOGGER.info("Received message {}.", validationInput.toString());
+    }
+
 }
